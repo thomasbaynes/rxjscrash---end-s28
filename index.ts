9307e13d4fdaf9e12c535c05d10c35290e9000e8
thomasbaynes <tom@workfacta.com>
2021-08-26 14:14:54 +0930

    1 file modified

--- index.ts
+++ index.ts
@@ -343,6 +343,36 @@ import { fromEvent } from 'rxjs';
 //   console.log('Unsubscribe');
 // }, 5000);
 
-
 //Session 5 // Session 41
+//forkJoin
 
+import { ajax, AjaxResponse } from 'rxjs/ajax';
+
+import { forkJoin } from 'rxjs';
+
+const randomName$ = ajax('https://random-data-api.com/api/name/random_name');
+
+const randomNation$ = ajax(
+  'https://random-data-api.com/api/nation/random_nation'
+);
+
+const randomFood$ = ajax('https://random-data-api.com/api/food/random_food');
+
+randomName$.subscribe(ajaxResponse =>
+  console.log(ajaxResponse.response.first_name)
+);
+
+randomNation$.subscribe(ajaxResponse =>
+  console.log(ajaxResponse.response.capital)
+);
+
+randomFood$.subscribe(ajaxResponse => console.log(ajaxResponse.response.dish));
+
+forkJoin([randomName$, randomNation$, randomFood$]).subscribe(
+  ([nameAjax, nationAjax, foodAjax]) =>
+    console.log(
+      `${nameAjax.response.first_name} is from ${
+        nationAjax.response.capital
+      } and likes to eat ${foodAjax.response.dish}`
+    )
+);
