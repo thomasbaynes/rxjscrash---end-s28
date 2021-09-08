27dcd581318d8620bfc53d7b1c8172ff1e1802b3
thomasbaynes <tom@workfacta.com>
2021-09-08 13:21:46 +0930

    2 files modified

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { Observable, Subscriber } from 'rxjs';
+import { filter, Observable, Subscriber } from 'rxjs';
 
 // const observable$ = new Observable<string>(subscriber => {
 //   console.log('Observable Executed');
@@ -440,5 +440,32 @@ import { fromEvent, combineLatest } from 'rxjs';
 //   }
 // );
 
-
 //Section 6
+// Session 47
+//Filter Operator
+
+interface NewsItem {
+  category: 'Business' | 'Sports';
+  content: string;
+}
+
+const newsFeed$ = new Observable<NewsItem>(subscriber => {
+  setTimeout(
+    () => subscriber.next({ category: 'Business', content: 'A' }),
+    1000
+  );
+  setTimeout(() => subscriber.next({ category: 'Sports', content: 'B' }), 3000);
+  setTimeout(
+    () => subscriber.next({ category: 'Business', content: 'C' }),
+    4000
+  );
+  setTimeout(() => subscriber.next({ category: 'Sports', content: 'D' }), 6000);
+  setTimeout(
+    () => subscriber.next({ category: 'Business', content: 'E' }),
+    7000
+  );
+});
+
+newsFeed$
+  .pipe(filter(item => item.category === 'Sports'))
+  .subscribe(item => console.log(item));
